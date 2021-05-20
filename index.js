f07a1333d1e6c10c2d0249b879d47bc325459c22
AMITAYUSH2003 <75991180+AMITAYUSH2003@users.noreply.github.com>
2021-05-20 18:12:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+
+// let ggg = 55;
+// console.log(ggg ,toString);
+
+// let ttt = String(55);
+// console.log(ttt );
+
+// var bbb = Number(44);
+// console.log(bbb ,(typeof bbb));
+
+// const abc =String (34.098776);
+// console.log(abc , (typeof abc))
+
+// var abc =parseFloat(3678764.098776);
+// console.log(abc.toFixed(2), (typeof abc));
+
+
+
+// var htm = "hello ";
+// let js = "khushal";
+// console.log(htm + js);
+
+
+// var htmll = "hello";
+// let jss = "khushal";
+// console.log(htmll + ' ' + jss);
+
+// var html ='orange mango apple';
+
+
+// html = html.concat(' beri ' ,'kivi ', 'banana');
+// console.log(html.substring(1, 5));
+// console.log(html);
+
+// console.log(html.toUpperCase());
+
+// console.log(html.includes('lemon'));
+// console.log(html.includes('mango'));
+
+// console.log(html.endsWith('banana'))  
+// console.log(html.slice(4, 8));
+// console.log(html.slice(-8));
+
+
+
+// console.log(html.indexOf('p'));
+// console.log(html.lastIndexOf('p'));
+// console.log(html[9])
+
+// console.log(html.charAt('8'));
+// console.log(html.split(' '));
+// console.log(html.replace('apple', 'this'));
+
+// var animal1 = 'tiger';
+// var animal2 = 'lion';
+
+// let myhtml = `
+//                this is heading 
+//                best animal in world frist
+//                  is ${animal1} and ${animal2}
+// `;
+// document.body.innerHTML = myhtml;
+
+
+  
+
+
+
+
+
